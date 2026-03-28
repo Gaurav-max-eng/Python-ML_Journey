230ac570db0f10a2e1d537de4b292922b5ea4ca1
Gaurav <g8182976@gmail.com>
2026-03-28 16:30:31 +0530

    Lesson 1 added - data types

---
+++ practice1.py
@@ -0,0 +1,22 @@
+# Lesson 1 - Variables and Data Types
+# Name: Gaurav
+# Date: 28 March 2026
+
+name = "Gaurav"        # string (text)
+age = 20               # int (numbers)
+height = 5.8           # float (decimal)
+is_student = True      # bool (True or false)
+
+print("My name is:", name)
+print("My age is:", age)
+print("My height is:", height)
+print("Am i a student?", is_student)
+
+# type checking
+
+print("\nData types")
+print("name type:", type(name))
+print("age type:", type(age))
+print("height type:", type(height))
+print("is_student:", type(is_student))
+
